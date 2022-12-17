9df677e7982b88e057d728bf89ae106ced547690
Oleh Paryliak <o.paryliak@tenantcloud.com>
2022-12-17 14:05:33 +0200

    complete first lab

--- Lab1,2/Lab1,2.playground/Contents.swift
+++ Lab1,2/Lab1,2.playground/Contents.swift
@@ -10,7 +10,7 @@ import UIKit
 
 // 1) Task 1.
 // Input data, could be changed
-let message = "advancedencriptionstandart" // 26 letters
+let message = "advanceddecriptionstandart" // 26 letters
 print(message.count)
 let rowKeyWord = "crypto" // 6 letters
 let columnKeyWord = "world" // 5 letters
@@ -96,14 +96,71 @@ cryptoMessage
 // Decrypting process
 // To decrypt we need to build matrix with encrypted message and column, rows permutated keys
 
+let arrCryptoMessage = Array(cryptoMessage).map(String.init)
+
+var cryptoMatrix = Array(repeating: Array(repeating: "+", count: colums), count: rows)
+
+cryptoMatrix
+var cryptoMsCounter = 0
+for i in 0...colums-1 {
+    for j in 0...rows-1 {
+        if (i == 0 && j != 0) {
+            cryptoMatrix[j][i] = sortedRowKey[j-1]
+        } else if (j == 0 && i != 0) {
+            cryptoMatrix[j][i] = sortedColumnKey[i-1]
+        } else if (i != 0 && j != 0) {
+            cryptoMatrix[j][i] = arrCryptoMessage[cryptoMsCounter]
+            cryptoMsCounter += 1
+        }
+    }
+}
+
+cryptoMatrix
+
+// Now we need just run permutation with inversed order firstly columnPermutation then rawPermutation
 
 
+// perform column permutation
+var cryptoMatrixAfterColumnPermutation = Array(repeating: Array(repeating: "*", count: colums), count: rows)
+
+for i in 0...colums-1 {
+    for j in 0...rows-1 {
+        if i == 0 {
+            cryptoMatrixAfterColumnPermutation[j][i] = cryptoMatrix[j][i]
+        } else {
+            if let indexOfColumn = sortedColumnKey.firstIndex(of: zeroRow[i-1]) {
+                cryptoMatrixAfterColumnPermutation[j][i] = cryptoMatrix[j][indexOfColumn+1]
+            }
+        }
+    }
+}
+
+cryptoMatrixAfterColumnPermutation
 
 
+// perform row permutation
+var cryptoMatrixAfterRowPermutation = [[String]]()
+cryptoMatrixAfterRowPermutation.append(cryptoMatrixAfterColumnPermutation[0])
 
+for i in 1...rows-1 {
+    if let indexOfRow = sortedRowKey.firstIndex(of: zeroColumn[i-1]) {
+        print(indexOfRow)
+        cryptoMatrixAfterRowPermutation.append(cryptoMatrixAfterColumnPermutation[indexOfRow+1])
+    }
+}
 
+cryptoMatrixAfterRowPermutation
 
+// create message from matrix
+var decriptedMessage = ""
+for i in 1...colums-1 {
+    for j in 1...rows-1 {
+        decriptedMessage += cryptoMatrixAfterRowPermutation[j][i]
+    }
+}
 
+decriptedMessage
+// TODO: move permutation and other dublicated code in functions with params
 
 func additional(val: Int) -> Int {
     return val + 2
